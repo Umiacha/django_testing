dbecdace355e1fd2824d19ffb916f71c79947b04
Sulaev Vladimir <umiacha@yandex.ru>
2023-08-04 22:48:05 +0300

    Add content testing. No refactoring

--- ya_news/news/pytest_tests/conftest.py
+++ ya_news/news/pytest_tests/conftest.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+import json
+
+from random import randint
 
 import pytest
 
@@ -7,15 +10,24 @@ from news.models import News, Comment
 
 @pytest.fixture
 def author(django_user_model):
+    """
+    Return User model.
+    """
     return django_user_model.objects.create(username='Автор')
 
 @pytest.fixture
 def author_client(author, client):
+    """
+    Return Client instance for author.
+    """
     client.force_login(author)
     return client
 
 @pytest.fixture
 def news():
+    """
+    Generate and return post (News instance).
+    """
     return News.objects.create(
         title='Title',
         text='Text',
@@ -24,9 +36,37 @@ def news():
 
 @pytest.fixture
 def comment(author, news):
+    """
+    Generate and return comment
+    for news created by author (Comment instance).
+    """
     return Comment.objects.create(
         news=news,
         author=author,
         text='Текст комментария',
         created=datetime.now
     )
+
+
+@pytest.fixture
+def posts_for_pagination(db):
+    """
+    Create News objects from JSON and return list of it
+    """
+    with open('/home/umiacha/yp/dev/django_testing/ya_news/news/fixtures/news.json') as news_fixture:
+        news_dict = json.load(news_fixture)
+    return [News.objects.create(**news['fields']) for news in news_dict]
+
+
+@pytest.fixture
+def comments_for_post(news, author):
+    COMMENTS_FROM_AUTHOR = 7
+    return [
+        Comment.objects.create(
+            news=news,
+            author=author,
+            text=f'Текст комментария {comm_counter}',
+            created=datetime.now().replace(day=randint(1, 11))
+        )
+        for comm_counter in range(COMMENTS_FROM_AUTHOR)
+    ]
---
+++ ya_news/news/pytest_tests/test_content.py
@@ -0,0 +1,49 @@
+from http import HTTPStatus
+
+import pytest
+
+from pytest_django.asserts import assertRedirects
+
+from django.urls import reverse
+
+@pytest.mark.usefixtures('posts_for_pagination')
+def test_news_count_on_mainpage(client):
+    HOME_PAGE_URL_NAME = 'news:home'
+    NEWS_ON_PAGE = 10
+    url = reverse(HOME_PAGE_URL_NAME)
+    response = client.get(url)
+    news_list = response.context['object_list']
+    assert len(news_list) == NEWS_ON_PAGE
+
+
+@pytest.mark.usefixtures('posts_for_pagination')
+def test_news_sorting(client):
+    HOME_PAGE_URL_NAME = 'news:home'
+    url = reverse(HOME_PAGE_URL_NAME)
+    response = client.get(url)
+    news_list = [news for news in response.context['object_list']]
+    sorted_news = sorted(news_list, key=lambda post: post.date, reverse=True)
+    assert sorted_news == news_list
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures('comments_for_post')
+def test_comments_sorting(client, news):
+    url = reverse('news:detail', args=(news.id,))
+    response = client.get(url)
+    comments_list = [comment for comment in response.context['news'].comment_set.all()]
+    sorted_comments = sorted(comments_list, key=lambda x: x.created)
+    assert sorted_comments == comments_list
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'user_agent, expected_result',
+    [
+        (pytest.lazy_fixture('client'), False),
+        (pytest.lazy_fixture('author_client'), True)
+    ]
+)
+def test_page_contains_comment_form(news, user_agent, expected_result):
+    url = reverse('news:detail', args=(news.id,))
+    response = user_agent.get(url)
+    assert ('form' in response.context) == expected_result
